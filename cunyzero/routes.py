491794631dc55d86dcab15b446cff0aebb97d459
reazs <reaz.shakil@onegoalgraduation.org>
2021-10-06 12:17:52 -0400

    updated to the index.html file and added css style to it

--- cunyzero/routes.py
+++ cunyzero/routes.py
@@ -5,3 +5,13 @@ from flask import  render_template
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+
+# @app.route("/<name>")
+# def login(name):
+#     return render_template("login.html")
