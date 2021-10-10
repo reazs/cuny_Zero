6268a49a8e32326eca35cf2c531fae75b41762de
reazs <reaz.shakil@onegoalgraduation.org>
2021-10-10 12:41:52 -0400

    updated the signup.hmtl routes.py and main.css

--- cunyzero/routes.py
+++ cunyzero/routes.py
@@ -5,7 +5,7 @@ import datetime as dt
 current_year = dt.datetime.now().year
 
 @app.route("/")
-def index():
+def home():
     return render_template("index.html", year=current_year)
 
 
@@ -13,6 +13,11 @@ def index():
 def login():
     return render_template("login.html")
 
+
+
+@app.route("/signup")
+def signup():
+    return render_template("signup.html")
 
 # @app.route("/<name>")
 # def login(name):
