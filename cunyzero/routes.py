41e5739f9afb533181cb5954239c8efb66ebe3f4
reazs <reaz.shakil@onegoalgraduation.org>
2021-10-10 20:20:39 -0400

    fixed login and signup page

--- cunyzero/routes.py
+++ cunyzero/routes.py
@@ -1,11 +1,16 @@
 from cunyzero import app
 from flask import  render_template, request
 import datetime as dt
+from cunyzero import class_schedule
 
 current_year = dt.datetime.now().year
 
+
 @app.route("/")
 def home():
+    for course in class_schedule.classes:
+        print(course)
+
     return render_template("index.html", year=current_year)
 
 
@@ -17,19 +22,30 @@ def login():
         print(request.form["email"])
         print(request.form["password"])
 
-    return render_template("login.html")
+    return render_template("login_signup/login.html")
 
 
-@app.route("/signup", methods=["POST","GET"])
+@app.route("/signup", methods=["POST", "GET"])
 def signup():
-    if request.method == "POST":
-        print(request.form["f_name"])
-        print(request.form["l_name"])
-        print(request.form["email"])
-        print(request.form["bio-content"])
-        print(request.form["password"])
-    return render_template("signup.html")
+    # if request.method == "POST":
+    #     print(request.form["f_name"])
+    #     print(request.form["l_name"])
+    #     print(request.form["email"])
+    #     print(request.form["bio-content"])
+    #     print(request.form["password"])
+    return render_template("login_signup/register_state.html")
 
+
+
+@app.route("/signup_student")
+def student_signup():
+    return render_template("login_signup/student_signup.html")
+
+
+
+@app.route("/signup_staff")
+def staff_signup():
+    return render_template("login_signup/staff_signup.html")
 # @app.route("/<name>")
 # def login(name):
 #     return render_template("login.html")
